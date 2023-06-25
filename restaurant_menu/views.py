868cd75106fab9f057f1c2629f1c630be0202373
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-06-25 10:35:20 +0200

    context shown in index page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from .models import ItemModel
 from django.views import generic
@@ -6,6 +7,10 @@ from django.views import generic
 class MenuList(generic.ListView):
     queryset = ItemModel.objects.order_by("meal")
     template_name = "index.html"
+    
+    def get_context_data(self):
+        context = {"meals":"Pizza"}
+        return context
 
 class menuItemDetailView(generic.DetailView):
     model = ItemModel
