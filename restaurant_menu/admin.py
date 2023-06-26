3e537d5d02f6f019b5cc2732c4eebf49da33d32f
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-06-26 17:09:52 +0200

    register menuItem to admin page

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ItemModel
 
 # Register your models here.
+class menuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal","meal_type","status", )
+    list_filter = ("status","meal_type")
+    search_fields = ("meal","description","meal_type")
+
+admin.site.register(ItemModel,menuItemAdmin)
